3ad940bb4ada9b317412067424641e1fe690d57c
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-29 22:35:08 -0700

    ll

--- node.py
+++ node.py
@@ -1,6 +1,9 @@
 
 
 class Node:
-    def __init__(self, val):
-        self.val = val
+    def __init__(self, myval):
+        self.val = myval
         self.next = None
+
+    def get_val(self):
+        return self.val
