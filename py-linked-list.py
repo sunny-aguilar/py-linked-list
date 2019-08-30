49d0e5d1b717221073c3f419c900a5586c674754
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-30 13:48:15 -0700

    ll

--- py-linked-list.py
+++ py-linked-list.py
@@ -1,17 +1,17 @@
-import node
+import nodes
 
 
 def main():
-    node1 = node.Node(10)
-    node2 = node.Node(20)
-    node3 = node.Node(30)
-    node4 = node.Node(40)
+    node1 = nodes.Node(10)
+    node2 = nodes.Node(20)
+    node3 = nodes.Node(30)
+    node4 = nodes.Node(40)
 
     node1.next = node2
     node2.next = node3
     node3.next = node4
 
-    print('Current Nodes - - - - - - - -')
+    print('\nCurrent Nodes - - - - - - - -')
     show_values(node1)
     add_node(node1, 50)
     add_node(node1, 60)
@@ -33,14 +33,19 @@ def show_values(node):
             temp_node = temp_node.next
 
 
-def add_node(nodes, val):
-    if nodes is not None:
-        temp_node = nodes
+def add_node(node, val):
+    if node is not None:
+        temp_node = node
         while temp_node.next is not None:
             temp_node = temp_node.next
 
-        new_node = node.Node(val)
+        new_node = nodes.Node(val)
         temp_node.next = new_node
+
+
+def add_node_front(node, val):
+    if node is not None:
+        temp_node = node
 
 
 main()
