c846c8d10b382cf07fb697d94b0118b6b30ee15d
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-29 22:35:45 -0700

    ll

--- py-linked-list.py
+++ py-linked-list.py
@@ -5,7 +5,7 @@ def main():
     node1 = node.Node(10)
     node2 = node.Node(20)
     node3 = node.Node(30)
-    node4 = Node(40)
+    node4 = node.Node(40)
 
     node1.next = node2
     node2.next = node3
