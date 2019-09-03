9f303fcb66edc4c898930cd64d441b1e1c1ec5d3
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-02 22:26:33 -0700

    ll

--- py-linked-list.py
+++ py-linked-list.py
@@ -80,6 +80,7 @@ def delete_node(node):
     temp_node_next = node.next
     if node is not None:
         while temp_node is not None:
+            
             del temp_node
             temp_node_next = temp_node.next
     else:
